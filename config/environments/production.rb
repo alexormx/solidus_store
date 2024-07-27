d3999b66bfc80088f91b157d4063abf0565c0007
Alex Ortiz <alexormx@gmail.com>
2024-07-27 18:33:31 -0500

    fix tailwind compresor issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-
+  config.assets.css_compressor = nil
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
